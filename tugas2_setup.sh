dfbb3bb823ff5271580fd29b54330353232e1132
Galang Hanafi <galanghanafi8@gmail.com>
2021-12-20 22:06:52 +0700

    Update tugas2_setup.sh

--- tugas2_setup.sh
+++ tugas2_setup.sh
@@ -7,7 +7,7 @@ echo "Menginstall library..."
 pip install --quiet --upgrade scikit-image
 
 # File Download
-echo "Mendownload file contoh..."
+echo "Mendownload gambar dari https://people.math.sc.edu/Burkardt/data/tif/tif.html"
 wget --quiet -O eight.png "https://people.math.sc.edu/Burkardt/data/tif/eight.png"
 wget --quiet -O kids.tif "https://people.math.sc.edu/Burkardt/data/tif/kids.tif"
 
